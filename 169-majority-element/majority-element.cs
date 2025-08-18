3d1e34a725c4337f3996324638bced366eb0cd1f
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-18 15:45:51 -0400

    Time: 0 ms (100.00%) | Memory: 49.6 MB (90.51%) - LeetSync

--- 169-majority-element/majority-element.cs
+++ 169-majority-element/majority-element.cs
@@ -1,14 +1,32 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        var dic = new Dictionary<int,int>();
+        // var dic = new Dictionary<int,int>();
 
-        foreach(int num in nums)
+        // foreach(int num in nums)
+        // {
+        //     if(dic.ContainsKey(num))
+        //         dic[num] = dic[num]+1;
+        //     else
+        //         dic.Add(num,1);
+        // }
+        // return dic.OrderByDescending(e => e.Value).First().Key;
+        int el = nums[0];
+        int cunt =1;
+        for(int i=1;i < nums.Length;++i)
         {
-            if(dic.ContainsKey(num))
-                dic[num] = dic[num]+1;
+            if(nums[i] == el)
+                ++cunt;
+            else if(cunt == 0)
+            {
+                el=nums[i];
+                ++cunt;
+            }
             else
-                dic.Add(num,1);
+            {
+                --cunt;
+            }
         }
-        return dic.OrderByDescending(e => e.Value).First().Key;
+
+        return el;
     }
 }
