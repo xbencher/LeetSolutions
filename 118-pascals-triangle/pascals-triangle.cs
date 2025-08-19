3d033eeef9795264e435e6db7748dfebd0b179d4
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-19 15:39:58 -0400

    Time: 0 ms (100.00%) | Memory: 40.3 MB (66.38%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        var result = new List<IList<int>>();
+        for(int r= 1;r<=numRows;++r)
+        {
+            var tempList = new List<int>();
+            tempList.Add(1);
+            int ans=1;
+            for(int c =1;c<r;++c)
+            {
+                    ans=ans*(r-c);
+                    ans=ans/c;
+                    tempList.Add(ans);
+            }
+            result.Add(tempList);
+        }
+
+        return result;
+    }
+}
