70c06adccb6fa0e597acf0282d3a03f308013182
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-21 01:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (53.71%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int n = matrix[0].Length;
+
+        for(int i=0;i<n;++i)
+        {
+            for(int j=i+1;j<n;++j)
+            {
+                int val = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] =val;
+            }
+        }
+
+        for(int i=0;i<n;++i)
+        {
+            int j=0;
+            int k=n-1;
+            while(j<k)
+            {
+                int val = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = val;
+                j++;
+                k--;
+            }
+        }
+    }
+}
