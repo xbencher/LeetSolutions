32d8a35aeb6906059ebb822af88873e10e08def9
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-20 14:16:19 -0400

    Time: 0 ms (100.00%) | Memory: 46.6 MB (67.39%) - LeetSync

--- 75-sort-colors/sort-colors.cs
+++ 75-sort-colors/sort-colors.cs
@@ -1,32 +1,33 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        int cunt0 =0;
-        int cunt1=0;
-        int cunt2=0;
+       int mid=0;
+       int low =0;
+       int high = nums.Length-1;
 
-        for(int i=0;i<nums.Length;++i)
-        {
-            if(nums[i]== 0) ++cunt0;
-            else if(nums[i] == 1) ++cunt1;
-            else ++cunt2;
-        }
-
-        for(int i=0;i<nums.Length;++i)
-        {
-            if(cunt0 >0)
+       while(mid <= high)
+       {
+            if(nums[mid] == 0)
             {
-                nums[i]=0;
-                --cunt0;
+                int value = nums[low];
+                nums[low] =  nums[mid];
+                nums[mid] = value;
+                ++low;
+                if(mid <= low)
+                {
+                    mid++;
+                }
             }
-            else if(cunt1>0)
+            else if(nums[mid] ==1)
             {
-                nums[i]=1;
-                --cunt1;
+                    mid++;
             }
             else
-            { 
-                nums[i]=2;
+            {
+                int value = nums[high];
+                nums[high]=nums[mid];
+                nums[mid] = value;
+                high--;
             }
-        }
+       }
     }
 }
