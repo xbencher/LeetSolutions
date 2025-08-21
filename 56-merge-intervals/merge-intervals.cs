e267d1da561ef21af85607d7c6376021f248e124
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-22 02:29:15 +0530

    Time: 5 ms (60.40%) | Memory: 56.9 MB (41.90%) - LeetSync

--- 56-merge-intervals/merge-intervals.cs
+++ 56-merge-intervals/merge-intervals.cs
@@ -10,27 +10,64 @@ public class Solution {
             return cmp;
         });
 
-        for(int i=0;i<totalIntervals;++i)
+        int[] currentInterVal = new int[]{intervals[0][0], intervals[0][1]};
+        int initialPoint = 1;
+
+        if(totalIntervals == 1)
         {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            
-            if(res.Count!=0 && end <= res.Last()[1] )
-            {
-                continue;
-            }
-
-            for(int j = i+1;j<totalIntervals;++j)
-            {
-                if(intervals[j][0] <= end)
-                {
-                    end = Math.Max(end,intervals[j][1]);
-                }
-                else break;
-            }
-
-            res.Add(new int[]{start,end});
+            res.Add(currentInterVal);
         }
+
+        while(initialPoint<totalIntervals)
+        {
+            var start = intervals[initialPoint][0];
+            var end  = intervals[initialPoint][1];
+
+            if(start <= currentInterVal[1])
+            {
+                currentInterVal[1] = Math.Max(currentInterVal[1],end);
+                ++initialPoint;
+
+                if(initialPoint == totalIntervals)
+                {
+                    res.Add(currentInterVal);
+                }
+            }
+            else
+            {
+                 res.Add(currentInterVal);
+                ++initialPoint;
+                currentInterVal = new int[]{start,end}; 
+
+                if(initialPoint == totalIntervals)
+                {
+                    res.Add(currentInterVal);
+                }
+            }
+        }
+
+
+        // for(int i=0;i<totalIntervals;++i)
+        // {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+            
+        //     if(res.Count!=0 && end <= res.Last()[1] )
+        //     {
+        //         continue;
+        //     }
+
+        //     for(int j = i+1;j<totalIntervals;++j)
+        //     {
+        //         if(intervals[j][0] <= end)
+        //         {
+        //             end = Math.Max(end,intervals[j][1]);
+        //         }
+        //         else break;
+        //     }
+
+        //     res.Add(new int[]{start,end});
+        // }
 
         return res.ToArray();
     }
