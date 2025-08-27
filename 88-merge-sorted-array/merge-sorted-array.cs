6cca4c27571445dba9eb47889d8744eb93f86d34
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-28 01:50:15 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (90.20%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cs
+++ 88-merge-sorted-array/merge-sorted-array.cs
@@ -1,16 +1,38 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
+       int num1i =m-1; // place till sorted array belongs
+       int num2j =n-1; // Place till sorted array belongs
 
-        int n1 =m;
-        int n2 = 0;
+        int size  = m+n-1;
 
-        while(n2<n)
+        while(num1i >=0 && num2j >=0)
         {
-            nums1[n1] = nums2[n2];
-            n1++;
-            n2++;
+            if(nums1[num1i] > nums2[num2j])
+            {
+                nums1[size] = nums1[num1i];
+                --num1i;
+                --size;
+            }
+            else
+            {
+                nums1[size] = nums2[num2j];
+                --num2j;
+                --size;
+            }
         }
 
-        Array.Sort(nums1);
+        while(num1i >=0)
+        {
+            nums1[size] = nums1[num1i];
+            --size;
+            --num1i;
+        }
+
+        while(num2j >=0)
+        {
+            nums1[size] = nums2[num2j];
+            --size;
+            --num2j;
+        }
     }
 }
