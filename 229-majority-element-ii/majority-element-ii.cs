a3c37ef89f6e70ecc7e3c7721a508856e67fcd45
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-08-31 03:29:56 +0530

    Time: 1 ms (100.00%) | Memory: 53.3 MB (85.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cs
+++ 229-majority-element-ii/majority-element-ii.cs
@@ -1,27 +1,50 @@
 public class Solution {
     public IList<int> MajorityElement(int[] nums) {
-        var dict = new Dictionary<int,int>();
+       
         List<int> list = new List<int>();
-        var n = (nums.Length)/3;
-        foreach(int key in nums)
+        int n = nums.Length;
+        int count1 =0;
+        int count2=0;
+        int el1=int.MinValue;
+        int el2=int.MinValue+1;
+
+        for(int i=0;i<n;++i)
         {
-            if(dict.ContainsKey(key))
+            if(count1 == 0 && nums[i]!= el2)
             {
-                dict[key]+=1;
+                count1 = 1;
+                el1=nums[i];
             }
-            else
+            else if(count2 == 0 && nums[i] != el1)
             {
-                dict.Add(key,1);
+                count2 = 1;
+                el2=nums[i];
+            }
+            else if(nums[i] == el1)
+            {
+                ++count1;
+            }
+            else if(nums[i] == el2)
+            {
+                ++count2;
+            }
+            else 
+            {
+                --count1;
+                --count2;
             }
         }
 
-        foreach(var item in dict)
+        count1=0;count2=0;
+
+        foreach(int item in nums)
         {
-            if(item.Value >n)
-            {
-                list.Add(item.Key);
-            }
+            if(item == el1)++count1;
+            if(item == el2)++count2;
         }
+
+        if(count1 > n/3) list.Add(el1);
+        if(count2 > n/3) list.Add(el2);
 
         return list;
     }
