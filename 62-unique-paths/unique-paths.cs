a4200b28e802da32725caa24b38dbeb7c9f7474c
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-09-02 00:05:43 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (54.96%) - LeetSync

--- 62-unique-paths/unique-paths.cs
+++ 62-unique-paths/unique-paths.cs
@@ -1,27 +1,14 @@
 public class Solution {
-    private int[,] dp;
     public int UniquePaths(int m, int n) {
-        dp = new int[m, n];
 
-        for(int i=0;i<m;++i)
+        int N = m+n-2;
+        int r= m-1;
+        double res =1;
+
+        for(int i=1;i<=r;++i)
         {
-            for(int j=0;j<n;++j)
-            {
-                dp[i,j] = -1;
-            }
+            res = res * (N-r +i)/i;
         }
-        return CountPath(0,0,m,n, dp);
-    }
-
-    public int CountPath(int i, int j, int m, int n, int[,] dptable)
-    {
-        if(i == m-1 && j==n-1) return 1;
-
-        if(i > m-1 || j > n-1) return 0;
-
-        if(dptable[i,j] != -1) return dptable[i,j];
-
-return dptable[i,j] = (CountPath(i+1, j, m, n,dptable)
-+ CountPath(i, j+1, m, n,dptable));
+        return (int)res;
     }
 }
