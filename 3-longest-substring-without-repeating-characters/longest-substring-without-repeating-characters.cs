d850ac8b49539e0885f6710925d255abc41fb530
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-09-11 02:16:41 +0530

    Time: 6 ms (57.12%) | Memory: 43.6 MB (88.88%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> set = new HashSet<char>();
+        int maxSub = 0;
+        int l=0;
+        for(int r=0;r<s.Length;++r)
+        {
+            while(l<r && set.Contains(s[r]))
+            {
+                set.Remove(s[l]);
+                ++l;
+            }
+
+            set.Add(s[r]);
+            int len = r-l+1;
+            maxSub = Math.Max(len, maxSub);
+
+        }
+
+        return maxSub;
+    }
+}
