b4dead2fa590e83d500ab7ba2f5484c3ece5b098
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2025-09-22 16:54:05 -0400

    Time: 3 ms (97.79%) | Memory: 68.8 MB (71.01%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cs
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        //dummy head
+        // find mid-node
+        // from mid reverse link list
+        //start compare with head and mid
+        ListNode slow= head;
+        ListNode fast= head;
+
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow = ReverseList(slow);
+
+        while(slow != null) 
+        {
+            if(head.val != slow.val) return false;
+
+            slow = slow.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode dummy = null;
+        while(head != null)
+        {
+            ListNode nextNode = head.next;
+            head.next = dummy;
+            dummy = head;
+            head =nextNode;
+        }
+       return dummy;
+    }
+}
