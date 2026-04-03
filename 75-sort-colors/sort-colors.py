97fd46b8f64089ec0ee881dad48d9b5a3eb02a11
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2026-04-03 12:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (99.20%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -2,24 +2,19 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         i=0
         j=0
-        k=0
+        k=len(nums)-1
 
-        for x in nums:
-            if x==0:
+        while j<=k:
+            if nums[j]==2:
+                temp=nums[j]
+                nums[j]=nums[k]
+                nums[k]=temp
+                k-=1
+            elif nums[j]==0:
+                temp=nums[j]
+                nums[j]=nums[i]
+                nums[i]=temp
                 i+=1
-            elif x==1:
                 j+=1
             else:
-                k+=1
-
-        
-        for x in range(len(nums)):
-            if i>0:
-                nums[x]=0
-                i-=1
-            elif j>0:
-                nums[x]=1
-                j-=1
-            else:
-                nums[x]=2
-                k-=1
+                j+=1
