846247024d2e4adfe90e3d35656b81d2c42c5cc5
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2026-04-15 18:58:06 +0530

    Time: 1 ms (100.00%) | Memory: 64.8 MB (6.25%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cs
+++ 53-maximum-subarray/maximum-subarray.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
-
-       int max = int.MinValue;
-       int sum =0; 
-       
-        for(int i=0;i<nums.Length;++i)
+        int max =int.MinValue;
+        int sum =0;
+        for(int i = 0;i<nums.Length;++i)
         {
             sum += nums[i];
-            max = Math.Max(sum, max);
+            
+            if(sum > max)
+                 max=sum;
+                 
             if(sum < 0)
             {
-              sum =0;
+                sum = 0;
             }
+
         }
 
         return max;
