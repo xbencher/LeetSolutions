457c4843803c47e3ee04026209d9c03ebc183200
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2026-04-16 15:13:11 +0530

    Time: 1 ms (99.90%) | Memory: 48.6 MB (99.39%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cs
+++ 13-roman-to-integer/roman-to-integer.cs
@@ -3,63 +3,40 @@ public class Solution {
         
         int Number =0;
         int LengthString = s.Length-1;
-        char prev = s[LengthString];
+        int prev = 0;
 
         for(int i = LengthString;i>=0;--i)
         {
-            char value= s[i];
-            
-            switch(value)
+            int current= GetValue(s[i]);
+
+            if(current < prev)
             {
-                case 'I' :
-                    if(prev == 'V' || prev == 'X')
-                    {
-                        Number -= 1;
-                    }
-                    else
-                    {
-                         Number += 1;
-                    }
-                    break;
-                case 'V' :
-                    Number += 5;
-                    break;
-                case 'X' :
-                    if(prev == 'L' || prev == 'C')
-                    {
-                        Number -= 10;
-                    }
-                    else
-                    {
-                         Number += 10;
-                    }
-                    break;
-                case 'L' :
-                    Number += 50;
-                    break;
-                case 'C' :
-                    if(prev == 'D' || prev == 'M')
-                    {
-                        Number -= 100;
-                    }
-                    else
-                    {
-                         Number += 100;
-                    }
-                    break;
-                case 'D' :
-                    Number += 500;
-                    break;
-                case 'M' :
-                    Number += 1000;
-                    break;
-                default :
-                    break;
+                Number -=current;
+            }
+            else
+            {
+                Number +=current;
             }
 
-            prev = value;
+            prev = current;
         }
 
         return Number;
+    }
+
+
+    private int GetValue(char c)
+    {
+        return c switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0
+        };
     }
 }
