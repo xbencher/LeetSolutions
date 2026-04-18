ef625725a4324737735157e6c1b7d6fbdc3f124f
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2026-04-19 04:04:59 +0530

    Time: 4 ms (11.53%) | Memory: 62.9 MB (65.67%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -17,50 +17,6 @@ public class Solution {
             suffix *= nums[i+1];
             ans[i] *= suffix;
         }
-
-        // add suffix
-
         return ans;
     }
-
-
-
-    // public int[] ProductExceptSelf(int[] nums) {
-    //     int alength = nums.Length;
-    //     int[] prefix = new int[alength];
-    //     int[] sufixfix = new int[alength];
-    //     int[] ans = new int[alength]; 
-    //     int count =1;
-    //     for(int i =0;i<prefix.Length;++i)
-    //     {
-    //         count = count*nums[i];
-    //         prefix[i] = count;
-    //     }
-
-    //      count=1;
-
-    //     for(int j=sufixfix.Length-1;j>=0;--j)
-    //     {
-    //         count = count*nums[j];
-    //         sufixfix[j]=count;
-    //     }
-
-    //     for(int k=0;k<ans.Length;++k)
-    //     {
-    //         if(k==0)
-    //         {
-    //              ans[k] = sufixfix[k+1];
-    //         }
-    //         else if(k==ans.Length-1)
-    //         {
-    //             ans[k] = prefix[k-1];
-    //         }
-    //         else
-    //         {
-    //             ans[k] = prefix[k-1] * sufixfix[k+1];
-    //         }
-    //     }
-
-    //     return ans;
-    // }
 }
