89bf309d2d4c5924f4712fc530e8a777eb13c9ec
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2026-04-19 04:04:23 +0530

    Time: 4 ms (11.53%) | Memory: 63.1 MB (54.51%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -1,40 +1,66 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
         int alength = nums.Length;
-        int[] prefix = new int[alength];
-        int[] sufixfix = new int[alength];
         int[] ans = new int[alength]; 
-        int count =1;
-        for(int i =0;i<prefix.Length;++i)
+        Array.Fill(ans, 1);
+
+        // fill prefix in array
+        for(int i=1;i<alength;++i)
         {
-            count = count*nums[i];
-            prefix[i] = count;
+            ans[i]=ans[i-1]*nums[i-1];
         }
 
-         count=1;
+        int suffix=1;
 
-        for(int j=sufixfix.Length-1;j>=0;--j)
+        for(int i =alength-2;i>=0;i--)
         {
-            count = count*nums[j];
-            sufixfix[j]=count;
+            suffix *= nums[i+1];
+            ans[i] *= suffix;
         }
 
-        for(int k=0;k<ans.Length;++k)
-        {
-            if(k==0)
-            {
-                 ans[k] = sufixfix[k+1];
-            }
-            else if(k==ans.Length-1)
-            {
-                ans[k] = prefix[k-1];
-            }
-            else
-            {
-                ans[k] = prefix[k-1] * sufixfix[k+1];
-            }
-        }
+        // add suffix
 
         return ans;
     }
+
+
+
+    // public int[] ProductExceptSelf(int[] nums) {
+    //     int alength = nums.Length;
+    //     int[] prefix = new int[alength];
+    //     int[] sufixfix = new int[alength];
+    //     int[] ans = new int[alength]; 
+    //     int count =1;
+    //     for(int i =0;i<prefix.Length;++i)
+    //     {
+    //         count = count*nums[i];
+    //         prefix[i] = count;
+    //     }
+
+    //      count=1;
+
+    //     for(int j=sufixfix.Length-1;j>=0;--j)
+    //     {
+    //         count = count*nums[j];
+    //         sufixfix[j]=count;
+    //     }
+
+    //     for(int k=0;k<ans.Length;++k)
+    //     {
+    //         if(k==0)
+    //         {
+    //              ans[k] = sufixfix[k+1];
+    //         }
+    //         else if(k==ans.Length-1)
+    //         {
+    //             ans[k] = prefix[k-1];
+    //         }
+    //         else
+    //         {
+    //             ans[k] = prefix[k-1] * sufixfix[k+1];
+    //         }
+    //     }
+
+    //     return ans;
+    // }
 }
