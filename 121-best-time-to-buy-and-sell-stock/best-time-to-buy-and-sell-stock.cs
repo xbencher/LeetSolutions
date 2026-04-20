68e05ff9ee18b461c35b19ee6411883bf697804a
Abhishek Singh <65116299+xbencher@users.noreply.github.com>
2026-04-20 22:53:19 +0530

    Time: 1 ms (99.86%) | Memory: 57.4 MB (32.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -1,12 +1,19 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int minPrice = int.MaxValue;
-        int maxProfit = 0;
-        foreach (int price in prices){
-            minPrice = Math.Min(price, minPrice);
-            maxProfit = Math.Max(maxProfit, price - minPrice);
+        int min =prices[0];
+        int profit =0;
+
+        for(int i =1;i<prices.Length;++i)
+        {
+            if(prices[i]<=min)
+            {
+                min =prices[i];
+            }
+
+            int cprofit = prices[i]-min;
+            profit= Math.Max(profit,cprofit);
         }
 
-        return maxProfit;
+        return profit;
     }
 }
